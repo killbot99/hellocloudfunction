4610680592f7c9299dd027f784efc463462cf020
david homan <david.homan@wpengine.com>
2020-11-03 09:13:56 -0600

    get rid of main.

--- hello.go
+++ hello.go
@@ -10,9 +10,4 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("Hello World! %s", time.Now())
 }
 
-func main() {
-	var w http.ResponseWriter
-	var r http.Request
-	Hello(r, *w)
-}
 
